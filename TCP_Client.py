ec444ddc2176a57fa2c689849c4eac65e070f866
pvichar <pvichar@ncsu.edu>
2015-10-08 18:21:37 -0400

    Read server response and print
    added a method to check the server response

--- TCP_Client.py
+++ TCP_Client.py
@@ -20,7 +20,10 @@ def createLISTMessage(serverAddress, serverPort):
     listMessage = 'LIST ALL' + ' ' + 'P2P-CI/1.0\r\n' + 'Host: ' + serverAddress + '\r\n' + 'Port: ' + serverPort + '\r\n'
     print(listMessage)
 
+def checkServerResponse():
+    
 
+   
 rfcNumber = '237'
 rfcTitle = 'Day 0: IP Project'
 serverAddress = '10.139.58.194'
@@ -32,6 +35,8 @@ s.connect((serverAddress, serverPort))
 s.send(createADDMessage(rfcNumber, serverAddress, serverPort, rfcTitle))
 s.send(createLOOKUPMessage(rfcNumber, serverAddress, serverPort, rfcTitle))
 s.send(createLISTMessage(serverAddress, serverPort))
-#data = s.recv(BUFFER_SIZE)
+#serverResponse = s.recv(bufferSize)
+#serverStatus = checkServerResponse(serverResponse)
+#print(serverResponse)
 s.close()
 #print "received data:", data
