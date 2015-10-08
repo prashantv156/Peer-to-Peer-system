226386b2dacf6796b97306b07d821b8c964398b8
asood3 <asood3@ncsu.edu>
2015-10-08 16:55:23 -0400

    Adding IP Address and Port

--- TCP_Server.py
+++ TCP_Server.py
@@ -8,5 +8,7 @@ print ('The server is ready to receive')
 while 1:
      connectionSocket, addr = serverSocket.accept()
      sentence = connectionSocket.recv(1024)
+     print(addr)
      print(sentence)
+
 connectionSocket.close()
