ef8e35a94a0dc2a64c0c818e32e6aefa567552a5
Anand Varma <anand.pyro@gmail.com>
2015-10-08 18:17:20 -0400

    Fixed Client: createMessage functions
    - returns instead of print
    - convert a few int to strings
    - changed serverAddress to “localhost”

--- TCP_Client.py
+++ TCP_Client.py
@@ -1,37 +1,31 @@
 import socket
 
 def createADDMessage(rfcNumber, serverAddress, serverPort, rfcTitle):
-
-    
     addMessage = 'ADD' + ' ' + 'RFC' + ' ' + rfcNumber + ' ' + 'P2P-CI/1.0\r\n' + 'Host: ' + serverAddress + '\r\n' + 'Port: ' + serverPort + '\r\n' + 'Title: ' + rfcTitle + '\r\n'
-    print(addMessage)
+    return (addMessage)
     
 
 def createLOOKUPMessage(rfcNumber, serverAddress, serverPort, rfcTitle):
-
-    
     lookupMessage = 'LOOKUP' + ' ' + 'RFC' + ' ' +  rfcNumber + ' ' + 'P2P-CI/1.0\r\n' + 'Host: ' + serverAddress + '\r\n' + 'Port: ' + serverPort + '\r\n' + 'Title: ' + rfcTitle + '\r\n'
-    print(lookupMessage)
+    return (lookupMessage)
 
      
 def createLISTMessage(serverAddress, serverPort):
-
-    
     listMessage = 'LIST ALL' + ' ' + 'P2P-CI/1.0\r\n' + 'Host: ' + serverAddress + '\r\n' + 'Port: ' + serverPort + '\r\n'
-    print(listMessage)
+    return (listMessage)
 
 
 rfcNumber = '237'
 rfcTitle = 'Day 0: IP Project'
-serverAddress = '10.139.58.194'
+serverAddress = 'localhost'
 serverPort = 12000
 bufferSize = 4096
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((serverAddress, serverPort))
-s.send(createADDMessage(rfcNumber, serverAddress, serverPort, rfcTitle))
-s.send(createLOOKUPMessage(rfcNumber, serverAddress, serverPort, rfcTitle))
-s.send(createLISTMessage(serverAddress, serverPort))
+s.send(createADDMessage(rfcNumber, serverAddress, str(serverPort), rfcTitle))
+s.send(createLOOKUPMessage(rfcNumber, serverAddress, str(serverPort), rfcTitle))
+s.send(createLISTMessage(serverAddress, str(serverPort)))
 #data = s.recv(BUFFER_SIZE)
 s.close()
 #print "received data:", data
