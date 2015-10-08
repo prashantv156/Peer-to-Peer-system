75f3b483e8c8fe7b6ca118de14a6675e3ce62e6e
pvichar <pvichar@ncsu.edu>
2015-10-08 19:59:07 -0400

    Added dictionary for RFC's and a method to open the socket
    Added a dictionary for holding RFCs and their titles for a client

--- TCP_Client.py
+++ TCP_Client.py
@@ -1,5 +1,28 @@
 import socket
 
+def openTCPSocketAndGetResponse(serverAddress, serverPort, bufferSize, clientRfcDictionary):
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((serverAddress, serverPort))
+    rfcCount = len(clientRfcDictionary.keys())
+
+    # if client has no rfc's ask server to list all rfcs
+    if rfcCount == 0:     
+       s.send(createLISTMessage(serverAddress, serverPort))
+
+    # if client has RFC's then add to the server
+    if rfcCount > 0:
+
+       for key in clientRfcDictionary.keys():
+           rfcNumber = key
+           rfcTitle = clientRfcDictionary[rfcNumber]
+           s.send(createADDMessage(rfcNumber, serverAddress, serverPort, rfcTitle))
+       
+##    serverResponse = s.recv(bufferSize)
+##    print(serverResponse)
+    s.close()
+
+
 def createADDMessage(rfcNumber, serverAddress, serverPort, rfcTitle):
     addMessage = 'ADD' + ' ' + 'RFC' + ' ' + str(rfcNumber) + ' ' + 'P2P-CI/1.0\r\n' + 'Host: ' + serverAddress + '\r\n' + 'Port: ' + str(serverPort) + '\r\n' + 'Title: ' + rfcTitle + '\r\n'
     return (addMessage)
@@ -15,22 +38,20 @@ def createLISTMessage(serverAddress, serverPort):
     return (listMessage)
 
 def checkServerResponse():
-    
+    pass
 
-   
-rfcNumber = 237
-rfcTitle = 'Day 0: IP Project'
+
 serverAddress = 'localhost'
 serverPort = 12000
 bufferSize = 4096
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((serverAddress, serverPort))
-s.send(createADDMessage(rfcNumber, serverAddress, serverPort, rfcTitle))
-s.send(createLOOKUPMessage(rfcNumber, serverAddress, serverPort, rfcTitle))
-s.send(createLISTMessage(serverAddress, serverPort))
-#serverResponse = s.recv(bufferSize)
-#serverStatus = checkServerResponse(serverResponse)
-#print(serverResponse)
-s.close()
-#print "received data:", data
+#create initial dictionary of all RFCs with their titles that a client has
+clientRfcDictionary = {}
+clientRfcDictionary[413] = 'Day 0 - IP Project'
+clientRfcDictionary[250] = 'Day 1 - IP Project'
+clientRfcDictionary[758] = 'Day 2 - IP Project'
+
+#open a TCP Socket, send a request and print the response
+openTCPSocketAndGetResponse(serverAddress, serverPort, bufferSize, clientRfcDictionary)
+
+
