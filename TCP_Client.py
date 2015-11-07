93a4933666caaf8c1361999eb67b1d881cb48bb4
pvichar <pvichar@ncsu.edu>
2015-11-06 19:06:12 -0500

    Read ADD/LOOKUP and LIST from command line
    Read ADD/LOOKUP and LIST  from command line and print the response

--- TCP_Client.py
+++ TCP_Client.py
@@ -1,44 +1,59 @@
 import socket
 from C2S_Protocol import *
+from threading import Thread
+import signal
+import sys
 
-def openTCPSocketAndGetResponse(serverAddress, serverPort, bufferSize, clientRfcDictionary):
+def openTCPSocketAndGetResponse(serverAddress, serverPort, bufferSize, command):
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((serverAddress, serverPort))
-    rfcCount = len(clientRfcDictionary.keys())
-
-    # if client has no rfc's ask server to list all rfcs
-    if rfcCount == 0:     
+        
+    if command == 'LIST':     
        s.send(createLISTMessage(serverAddress, serverPort))
        serverResponse = s.recv(bufferSize)
-       print serverResponse
+       print "<Resp>", serverResponse, "</Resp>"
 
-     
-    # if client has RFC's then add to the server
     
-    if rfcCount > 0:
-       
-       for key in clientRfcDictionary.keys():
-           rfcNumber = key
-           rfcTitle = clientRfcDictionary[rfcNumber]
-           print  rfcNumber, rfcTitle
-           s.send(createADDMessage(rfcNumber, serverAddress, serverPort, rfcTitle))
-           serverResponse = s.recv(bufferSize)
-           print "<Resp>", serverResponse, "</Resp>"
+    if command == 'ADD':
+        clientRfcDictionary = createClientDictionary()
+        for key in clientRfcDictionary.keys():
+
+            rfcNumber = key
+            rfcTitle = clientRfcDictionary[rfcNumber]
+            print  rfcNumber, rfcTitle
+            s.send(createADDMessage(rfcNumber, serverAddress, serverPort, rfcTitle))
+            serverResponse = s.recv(bufferSize)
+            print "<Resp>", serverResponse, "</Resp>"
+
+
+    if command == 'LOOKUP':
+
+        s.send(createLISTMessage(serverAddress, serverPort))
+        serverResponse = s.recv(bufferSize)
+        print "<Resp>", serverResponse, "</Resp>"
+        
            
  
     s.close()
     print "Socket Closed."
 
 
-serverAddress = 'localhost'
-serverPort = 12000
-bufferSize = 4096
-clientRfcDictionary = createClientDictionary()
+def clientToServer():
+
+    serverAddress = 'localhost'
+    serverPort = 12000
+    bufferSize = 4096
+
+    command = raw_input('Enter ADD/LOOKUP/LIST : ')
+
+    #open a TCP Socket, send a request and print the response
+    openTCPSocketAndGetResponse(serverAddress, serverPort, bufferSize, command)
+ 
+
+print 'the client is up and running'
+client = Thread(target=clientToServer)
+client.start()
+
     
-#open a TCP Socket, send a request and print the response
-openTCPSocketAndGetResponse(serverAddress, serverPort, bufferSize, clientRfcDictionary)
-
-
-
 
