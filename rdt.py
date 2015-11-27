5a50b4f7f1ac51899b65941a2ba3da4f32604fa7
pwang13 <pwang13@ncsu.edu>
2015-11-27 16:20:57 -0500

    add checksum function in rdt

--- rdt.py
+++ rdt.py
@@ -8,6 +8,28 @@ import datetime  #Import date and time module
 
 MSS = 2048 - 8
 
+def carry_checksum_addition(num_1, num_2):
+    c = num_1 + num_2
+    return (c & 0xffff) + (c >> 16)
+
+
+# Calculate the checksum of the data only. Return True or False
+def calculate_checksum(message):
+    # print (message)
+    if (len(message) % 2) != 0:
+        message += bytes("0")
+
+    checksum = 0
+    for i in range(0, len(message), 2):
+        my_message = str(message)
+        # print(my_message[i], ord(my_message[i]))
+        w = ord(my_message[i]) + (ord(my_message[i + 1]) << 8)
+        print(w)
+        checksum = carry_checksum_addition(checksum, w)
+    checksum = bin(checksum ^ 0xffff)
+    return checksum
+
+
 def rdt_send(s, msg, (peerAddress, peerPort)):
 
 	# split into MSS
@@ -18,6 +40,7 @@ def rdt_send(s, msg, (peerAddress, peerPort)):
 	# bad ack/ time out -> resend
 	# if all ack return
 	s.sendto(msg, (peerAddress, peerPort))
+	print(calculate_checksum(msg))
 
 def rdt_recv(s):
 	if True:
